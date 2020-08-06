029044b4958580a1d59a5968c6ef81515bb7e87a
Andre Melo <andre.fabian.melo@gmail.com>
2020-08-06 19:10:24 -0300

    Add pages navigation and style

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ function Landing() {
     navigate('GiveClasses')
   }
 
+  function handleNavigationToStudyPages() {
+    navigate('Study')
+  }
+
   return (
       <View style={styles.container}>
         <Image source={landingImage} style={styles.banner} />
@@ -29,7 +33,7 @@ function Landing() {
      
       
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton onPress={handleNavigationToStudyPages} style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
